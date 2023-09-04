34ae08181620f2b7352ea1704b5a56355fd7f3bd
zarazebra <sarah.iber.90@gmail.com>
2023-09-04 21:16:08 +0200

    Select player

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
-gameboard = [[" x", " x", " x"],
-             [" x", " x", " x"],
-             [" x", " x", " x"],
+title = """
+___    __     ___       __     ___  __   ___ 
+ |  | /  `     |   /\  /  `     |  /  \ |__  
+ |  | \__,     |  /~~\ \__,     |  \__/ |___ 
+                                             
+"""
+
+gameboard = [["  ", "  ", "  "],
+             ["  ", "  ", "  "],
+             ["  ", "  ", "  "],
              ]
 
-n = len(gameboard)
-for row in gameboard:
-    n -= 1
-    print(f" |".join(row))
-    if n != 0:
-        print("-----------")
+continue_game = True
+
+while continue_game:
+    print(title)
+    print("Welcome to the Tic Tac Toe Game")
+
+    player1 = input("Which player do you want to be - 'X' or 'O'?: ").upper()
+    if player1 == "X":
+        player2 = "O"
+    else:
+        player2 = "X"
+
+    n = len(gameboard)
+    for row in gameboard:
+        n -= 1
+        print(f" |".join(row))
+        if n != 0:
+            print("-----------")
+
+
 
