d1406b2cda81eb367bc92b64e918c35c549a07e8
zarazebra <sarah.iber.90@gmail.com>
2023-09-04 21:35:01 +0200

    Pick position P1

--- main.py
+++ main.py
@@ -28,6 +28,17 @@ while continue_game:
         print(f" |".join(row))
         if n != 0:
             print("-----------")
+    print("To play, you have to choose the location with row and column, e.g. first row third column - 13")
+    choice_player1 = input("Where do you wanna play?: ")
+    pos_player1 = list(choice_player1)
+    row_p1 = int(pos_player1[0]) - 1
+    column_p1 = int(pos_player1[1]) - 1
+    gameboard[row_p1][column_p1] = player1
 
-
+    n = len(gameboard)
+    for row in gameboard:
+        n -= 1
+        print(f" |".join(row))
+        if n != 0:
+            print("-----------")
 
